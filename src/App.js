2b52ef07965ae46ae292ef094c5a56c3f5c5fe68
Mahesh H C <109088120+Mahesh-HC@users.noreply.github.com>
2022-11-07 13:52:14 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BMICalculator from './Problem01/BMICalculator'
+
+function App() {
+  return (
+    <div>
+      <BMICalculator></BMICalculator>
+    </div>
+  );
+}
+
+export default App;
