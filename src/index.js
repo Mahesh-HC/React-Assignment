482c4e00ce2d6ce237b1553b93b20829e0119c8f
Mahesh H C <109088120+Mahesh-HC@users.noreply.github.com>
2022-11-21 03:51:43 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Problem02/Redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Provider store={store}>
+    <App />
+    </Provider>
+  </BrowserRouter>
+);
